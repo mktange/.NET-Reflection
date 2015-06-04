c92bb165f266026689a20c0237579e6e632a574f
mktange <mkt@delegate.dk>
2015-06-04 11:04:11 +0200

    Initial commit

---
+++ TestApp/Program.fs
@@ -0,0 +1,40 @@
+﻿
+open System
+open System.Reflection
+
+open Microsoft.FSharp.Reflection
+
+// Reflection method to invoke members without args
+let (?) x prop =
+    let flags = BindingFlags.GetProperty ||| BindingFlags.InvokeMethod
+    x.GetType().InvokeMember(prop, flags, null, x, [||])
+
+
+[<EntryPoint>]
+let main argv = 
+  
+  let src = __SOURCE_DIRECTORY__
+  let asm = 
+    Assembly.LoadFile(src + @"\..\TestLibrary\bin\Debug\TestLibrary.dll")
+  
+  // Create a new object
+  let ty = asm.GetType("TestLibrary.MainClass")
+  let instance = Activator.CreateInstance(ty)
+
+  // Get a string
+  let func = ty.GetMethod("GetSomeString")
+  let someStr = func.Invoke(instance, Array.empty) :?> string
+
+  // Get an object
+  let func = ty.GetMethod("GetSomeObject")
+  let someObj = func.Invoke(instance, Array.empty)  
+
+  // Print out all the values
+  printfn "Simple String: %s" someStr
+  printfn "Test: %s" (someObj?Test :?> string)
+  printfn "Hmm: %s" (someObj?Hmm :?> string)
+  printfn "blah: %d" (someObj?blah :?> int)
+  printfn "pop: %d" (someObj?pop :?> int)
+
+  Console.ReadKey() |> ignore
+  0
---
+++ TestLibrary/MainClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public class TransferClass
+    {
+        public string Test { get; set; }
+        public string Hmm { get; set; }
+        public int blah { get; set; }
+        public int pop { get; set; }
+
+        public TransferClass()
+        {
+            this.Test = "TEST";
+        }
+    }
+
+
+    public class MainClass
+    {
+        string someString;
+        TransferClass someClass;
+
+        public MainClass() {
+            this.someString = "Here is a simple string";
+
+            this.someClass = new TransferClass();
+            
+            this.someClass.blah = 1;
+            this.someClass.pop = 1337;
+            this.someClass.Test = "This is a test";
+            this.someClass.Hmm = "Woop";
+        }
+
+
+        public TransferClass GetSomeObject()
+        {
+            return this.someClass;
+        }
+
+        public string GetSomeString()
+        {
+            return this.someString;
+        }
+    }
+}
